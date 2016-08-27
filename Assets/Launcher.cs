4692ac49c536d74f939f710ba236ec5f8aa34cbf
benjaml <b.lefevre@rubika-edu.com>
2016-08-27 15:32:08 +0200

    Launcher first version

---
+++ Assets/Launcher.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Launcher : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () 
+    {
+        InvokeRepeating("launch", 0f, lauchRate);
+	}
+	
+    private void launch()
+    {
+        float posX = Random.Range(-spawnRange, spawnRange);
+        if(posX>0)
+            posX+=startSpawnRange;
+        else
+            posX-=startSpawnRange;
+        Debug.Log("instance position : " + posX);
+        transform.position = new Vector3(posX, transform.position.y, transform.position.z);
+        GameObject instance = Instantiate(prefabs[Random.Range(0, prefabs.Count)], transform.position, Quaternion.identity) as GameObject;
+        instance.GetComponent<DaddyBloc>().StartCoroutine("launch", curve);
+        //Debug.Log("instance position : " + instance.transform.position.x);
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Vector3 pointPosition = transform.position;
+        Gizmos.DrawSphere(pointPosition + Vector3.right * startSpawnRange, .5f);
+        Gizmos.DrawSphere(pointPosition - Vector3.right * startSpawnRange, .5f);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(pointPosition + Vector3.right * (startSpawnRange + spawnRange), .5f);
+        Gizmos.DrawSphere(pointPosition - Vector3.right * (startSpawnRange + spawnRange), .5f);
+    }
+
+    [SerializeField]
+    private AnimationCurve curve;
+
+    [SerializeField]
+    private float lauchRate;
+
+    [SerializeField]
+    private float spawnRange;
+
+    [SerializeField]
+    private float startSpawnRange;
+
+    [SerializeField]
+    private List<GameObject> prefabs = new List<GameObject>();
+}
--- Assets/__Cathedral/Scripts/DaddyBloc.cs
+++ Assets/__Cathedral/Scripts/DaddyBloc.cs
@@ -5,22 +5,78 @@ public class DaddyBloc : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+        GetComponent<Collider2D>().isTrigger = true;
+        foreach(Collider2D col in GetComponentsInChildren<Collider2D>())
+            col.isTrigger = true;
+        launchLenght = Mathf.Abs(transform.position.x) * 2f;
+        launchHeight = Random.Range(1f, 1.3f)*launchHeight;
 	}
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Destroy(gameObject.GetComponent<Collider2D>());
-        Destroy(gameObject.GetComponent<Rigidbody2D>());
-        foreach(Bloc bloc in transform.GetComponentsInChildren<Bloc>())
+        if (collision.transform.tag.Equals("Ground"))
         {
-            bloc.DetachBloc();
+            if(spawnOnGround)
+                spawnPrefabGround();
+            else 
+            {
+                // FX
+                Destroy(gameObject);
+            }
         }
-        Destroy(gameObject);
+        else 
+        {
+            Destroy(gameObject.GetComponent<Collider2D>());
+            Destroy(gameObject.GetComponent<Rigidbody2D>());
+            foreach(Bloc bloc in transform.GetComponentsInChildren<Bloc>())
+            {
+                bloc.DetachBloc();
+            }
+            Destroy(gameObject);
+        }
     }
+
+
+    private void spawnPrefabGround()
+    {
+        // spawn a prefab on ground Collision (house, tree)
+    }
+
+    IEnumerator launch(AnimationCurve curve)
+    {
+        
+        float startTime = Time.time;
+        Vector3 startPosition = transform.position;
+        launchLenght = Mathf.Abs(transform.position.x) * 2f;
+        float ratio;
+        bool right = transform.position.x > 0;
+        while((ratio = (Time.time-startTime)/launchDuration) <= 1)
+        {
+            float posX;
+            float posY;
+            if (right)
+            {
+                posX = ratio * -launchLenght;
+                posY = curve.Evaluate((1 - ratio)) * launchHeight;
+            }
+            else
+            {
+                posX = ratio* launchLenght;
+                posY = curve.Evaluate(ratio) * launchHeight;
+            } 
+            transform.position = startPosition + new Vector3(posX, posY, 0f);
+            yield return null;
+        }
+    }
+
+    [Header("Tweak")]
+    [SerializeField]
+    private float launchDuration;
+    [SerializeField]
+    private float launchHeight;
+    [SerializeField]
+    private float launchLenght;
+    [SerializeField]
+    private bool spawnOnGround = false;
+
 }
