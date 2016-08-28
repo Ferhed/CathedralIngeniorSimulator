2ea77df6b1e07317d32d45b0e714d508f262b62d
Ferhed <v.birembaut@rubika-edu.com>
2016-08-28 17:42:45 +0200

    add dezoom camera

--- Assets/__Cathedral/Scripts/Cathedral.cs
+++ Assets/__Cathedral/Scripts/Cathedral.cs
@@ -49,7 +49,7 @@ public class Cathedral : MonoBehaviour
         if (height > MaxHeight)
         {
             MaxHeight = height;
-            Debug.Log(MaxHeight);
+            CameraScript.Instance.ChangeDistance();
         }
     }
 
